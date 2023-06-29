987a438608b920ccfe75a579f8163182abae2cb9
hongsh429 <shh0429@kakao.com>
2023-06-29 12:16:16 +0900

    first commit

---
+++ src/test/java/com/firstweek/firstboad/FirstboadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstweek.firstboad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstboadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
